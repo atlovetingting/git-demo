1527ca9e7cf12276ddd5a7910931a5b4934d6058
wuqiongyu <wuqiongyu@world.com>
2022-05-21 21:48:10 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git2Test.java
+++ src/main/java/com/atguigu/git2Test.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class git2Test {
     public static void main(String[] args) {
-        System.out.println("hello git4");
+        System.out.println("hello hot-fix git4");
     }
 }
--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -4,6 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello zhouyexi");
-        System.out.println("hello hot-fix test");
+        System.out.println("hello hot-fix test1");
     }
 }
